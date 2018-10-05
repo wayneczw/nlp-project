505653a428eacf8ea75cec4a79e92f681cdf8577
jinpo <tony_zeng@Jinpos-MacBook-Pro.local>
2018-10-05 15:03:49 +0800

    update representative sentiment words

--- jp_sentiment_word_detection.py
+++ jp_sentiment_word_detection.py
@@ -8,7 +8,7 @@ import string
 import os
 import json
 
-from nltk import pos_tag
+from nltk import pos_tag,pos_tag_sents
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize, TweetTokenizer
@@ -19,6 +19,8 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sns
+import datetime
+
 
 # %matplotlib inline
 plt.style.use('seaborn-whitegrid')
@@ -32,29 +34,16 @@ pd.set_option('display.max_columns', 10)
 DEFAULT_DATA_FILE = "./data/sample_data.json"
 IMAGES_DIRECTORY = './images'
 DEFAULT_SEED = 42
-STOPWORDS = set(stopwords.words('english') + ["'s", "one", "use", "would", "get", "also"]) - {'not', 'no', 'won', 'more', 'above', 'very', 'against', 'again'}
+STOPWORDS = set(stopwords.words('english') + ["'ve','s", "one", "use", "would", "get", "also"]) - {'not', 'no', 'won', 'more', 'above', 'very', 'against', 'again'}
+PUNCTUATION = string.punctuation + '...'
+SPECIAL_TOKEN_DICT = {"n't": 'not'}
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 is_word = lambda token: not(EMOTICON_RE.search(token) or token in string.punctuation or token in STOPWORDS)
-NEG_SENT_BOUND_RE = re.compile(EMOTICON_RE.pattern +'|'.join(['.',':',';','!','?',','])
+
+NEG_SENT_BOUND_RE = re.compile(EMOTICON_RE.pattern +'|'+'|'.join(['\.','\:','\;','\!','\?','\,']))
 NEG_WORD_RE = re.compile(r"(?:^(?:never|no|nothing|nowhere|noone|none|not)$)")
 
-def review_sentiment(rating):
-    if rating <= 2:
-        return "negative"
-
-    elif rating == 3:
-        return "neutral"
-
-    else:
-        return "positive"
-
-def sentiment2target(sentiment):
-    return {
-        'negative': 0,
-        'neutral': 1,
-        'positive' : 2
-    }[sentiment]
 
 def main(data_file, seed):
 
@@ -78,7 +67,10 @@ def main(data_file, seed):
     # (3) convert all the other words in a sentence with negation words to NOT_word
 
     print(str(datetime.datetime.now()).split('.')[0] + ': Start processing tokenizing')
-    df['tokenizedSentences'] = df['sentences'].apply(lambda sentences: [tokenize(sentence, lower = True, remove_punc = True, remove_stopwords = True, convert_neg = True) for sentence in sentences])
+    df['tokenizedSentences'] = df['sentences'].apply(lambda sentences: [tokenize(sentence, lower = True, remove_punc = True, remove_stopwords = False, convert_neg = True) for sentence in sentences])
+
+    # df['posTagged'] = df['tokenizedSentences'].apply(lambda sentences: pos_tag_sents(sentences))
+
     df['tokens'] = df['tokenizedSentences'].apply(flatten)
     df['tokens'] = df['tokens'].apply(lambda tokens: list(set(tokens)))
     print(str(datetime.datetime.now()).split('.')[0] + ': Finish processing tokenizing')
@@ -86,8 +78,76 @@ def main(data_file, seed):
     df_positive = df[df['overall'] > 3]
     df_negative = df[df['overall'] < 3]
 
-    
+    words_positive = flatten(df_positive['tokens'])
+    words_negative = flatten(df_negative['tokens'])
 
+    df_top1000_words_positive = pd.DataFrame.from_dict(Counter(words_positive), orient='index').\
+                reset_index().rename(columns = {'index': 'Word', 0: 'Count'}).\
+                sort_values(['Count'], ascending = False).head(1000).\
+                reset_index().drop(columns = ['index'])
+    df_top1000_words_negative = pd.DataFrame.from_dict(Counter(words_negative), orient='index').\
+                reset_index().rename(columns = {'index': 'Word', 0: 'Count'}).\
+                sort_values(['Count'], ascending = False).head(1000).\
+                reset_index().drop(columns = ['index'])
+
+    top1000_words_positive_set = set(df_top1000_words_positive['Word'])
+    top1000_words_negative_set = set(df_top1000_words_negative['Word'])
+    common_words = set.intersection(top1000_words_positive_set,top1000_words_negative_set)
+
+    top20_words_positive = df_top1000_words_positive[~df_top1000_words_positive['Word'].isin(common_words)].nlargest(20,'Count')
+    top20_words_negative = df_top1000_words_negative[~df_top1000_words_negative['Word'].isin(common_words)].nlargest(20,'Count')
+
+    print(str(datetime.datetime.now()).split('.')[0] + ': top 20 positive words')
+    print(top20_words_positive)
+
+    #     2018-10-05 14:59:56: top 20 positive words
+    #              Word  Count
+    # 222  highly        6946
+    # 311  protects      4823
+    # 366  sturdy        4003
+    # 373  durable       3910
+    # 404  loves         3545
+    # 405  tablet        3544
+    # 446  amazing       3254
+    # 452  recommended   3208
+    # 459  protected     3143
+    # 460  pleased       3143
+    # 462  provides      3141
+    # 464  handy         3121
+    # 472  allows        3077
+    # 475  neg_problems  3068
+    # 478  bulk          3037
+    # 481  provided      3001
+    # 492  gives         2921
+    # 499  nicely        2879
+    # 502  led           2855
+    # 525  travel        2664
+
+    print(str(datetime.datetime.now()).split('.')[0] + ': top 20 negative words')
+    print(top20_words_negative)
+
+    # 2018-10-05 14:59:57: top 20 negative words
+    #               Word  Count
+    # 200  disappointed   1192
+    # 211  waste          1115
+    # 228  return         1061
+    # 231  neg_buy        1049
+    # 238  neg_recommend  1028
+    # 272  neg_worth      914
+    # 323  poor           771
+    # 327  returned       765
+    # 334  stopped        738
+    # 370  apart          657
+    # 374  neg_again      652
+    # 386  unfortunately  631
+    # 395  useless        603
+    # 426  send           562
+    # 433  refund         545
+    # 437  fell           543
+    # 458  broken         514
+    # 476  flimsy         489
+    # 478  horrible       489
+    # 497  neg_money      467
 #end def
 
 class ReviewTokenizer(TreebankWordTokenizer):
@@ -157,11 +217,13 @@ def tokenize(sentence, word_tokenizer = ReviewTokenizer(), stemmer = None, lower
 
     # remove punctuation tokens
     if remove_punc:
-        tokens = [token for token in tokens if token not in string.punctuation]
+        tokens = [token for token in tokens if token not in PUNCTUATION]
 
     # stem tokens
     if stemmer:
         tokens = [stemmer.stem(token) for token in tokens]
+
+    tokens = [SPECIAL_TOKEN_DICT[token] if SPECIAL_TOKEN_DICT.get(token, '') else token for token in tokens]
 
     return tokens
 #end def
@@ -182,7 +244,7 @@ def _convert_neg(tokens):
             i += 1
             while (i < n) :
                 token = tokens[i]
-                if not NEG_SENT_BOUND_RE(token):
+                if not NEG_SENT_BOUND_RE.match(token):
                     new_tokenized_list.append('neg_'+token)
                 else:
                     # end of negation
